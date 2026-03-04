a41ebb39e9660959a660c80a101370670d749c17
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-03-04 22:02:12 +0600

    Toggle functionality added for paybill section

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,32 +1,45 @@
-handleToggle("cashout", "none")
-handleToggle("transfer", "none")
-handleToggle("bonus", "none")
+handleToggle("cashout", "none");
+handleToggle("transfer", "none");
+handleToggle("bonus", "none");
+handleToggle("paybill", "none");
 
 document.getElementById("add-money-card").addEventListener("click", function () {
-    handleToggle("addmoney", "block")
-    handleToggle("cashout", "none")
-    handleToggle("transfer", "none")
-    handleToggle("bonus", "none")
-})
+    handleToggle("addmoney", "block");
+    handleToggle("cashout", "none");
+    handleToggle("transfer", "none");
+    handleToggle("bonus", "none");
+    handleToggle("paybill", "none");
+});
 
 document.getElementById("cash-out-card").addEventListener("click", function () {
-    handleToggle("addmoney", "none")
-    handleToggle("transfer", "none")
-    handleToggle("bonus", "none")
-    handleToggle("cashout", "block")
-})
+    handleToggle("addmoney", "none");
+    handleToggle("transfer", "none");
+    handleToggle("bonus", "none");
+    handleToggle("paybill", "none");
+    handleToggle("cashout", "block");
+});
 
 document.getElementById("transfer-money-card").addEventListener("click", function () {
-    handleToggle("addmoney", "none")
-    handleToggle("cashout", "none")
-    handleToggle("bonus", "none")
-    handleToggle("transfer", "block")
+    handleToggle("addmoney", "none");
+    handleToggle("cashout", "none");
+    handleToggle("bonus", "none");
+    handleToggle("paybill", "none");
+    handleToggle("transfer", "block");
 
-})
+});
 
 document.getElementById("get-bonus-card").addEventListener("click", function () {
-    handleToggle("bonus", "block")
-    handleToggle("addmoney", "none")
-    handleToggle("cashout", "none")
-    handleToggle("transfer", "none")
+    handleToggle("bonus", "block");
+    handleToggle("addmoney", "none");
+    handleToggle("cashout", "none");
+    handleToggle("transfer", "none");
+    handleToggle("paybill", "none");
+});
+
+document.getElementById("pay-bill-card").addEventListener("click", function () {
+    handleToggle("addmoney", "none");
+    handleToggle("cashout", "none");
+    handleToggle("transfer", "none");
+    handleToggle("bonus", "none");
+    handleToggle("paybill", "block");
 })
